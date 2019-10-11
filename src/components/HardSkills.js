37fd07c716c9a52cbc835be66edf3c2be1800463
07087646141 <lucas.souza2@poupex.com.br>
2019-10-11 15:40:10 -0300

    :arrow_up: :art: melhoria no css geral dos components

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -1,19 +1,78 @@
 import React from "react";
 import ReactStars from "react-stars";
-import {Box, Card, Grid} from "@material-ui/core";
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import {Box, Card, Grid, InputLabel} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import SendIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import SendIcon from "@material-ui/icons/Send";
 
 const HARDSKILLS = [
     {
+        'Desenvolvedor Javascript':
+            [
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
+            ]
+    },
+    {
         'Desenvolvedor Python':
             [
-                'testes unitários',
-                'Desenvolvimento Ágile',
-                'Boas Práticas',
-                'testes unitários',
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
             ]
-    }
+    },
+    {
+        'Desenvolvedor Ruby':
+            [
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
+            ]
+    },
+    {
+        'Desenvolvedor Ruby':
+            [
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
+            ]
+    },
+    {
+        'Desenvolvedor Python':
+            [
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
+            ]
+    },
+    {
+        'Desenvolvedor Java':
+            [
+                {'a': 0},
+                {'b': 0},
+                {'c': 0},
+                {'d': 0},
+                {'e': 0},
+                {'f': 0},
+            ]
+    },
 ]
 
 class HardSkills extends React.Component {
@@ -27,6 +86,23 @@ class HardSkills extends React.Component {
                     <Grid item>
                         <Card className='soft-skills'>
                             <h1>Habilidades <br/> Profissionais</h1>
+                            <FormControl variant="outlined">
+                                <InputLabel ref={'inputLabel'} htmlFor="outlined-age-native-simple">
+                                    Age
+                                </InputLabel>
+                                <Select
+                                    native
+                                    value={0}
+                                    onChange={console.log('putz')}
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'outlined-age-native-simple',
+                                    }}
+                                >
+                                    <option value="" />
+                                    <option value={11}>TESTE B</option>
+                                </Select>
+                            </FormControl>
                             <Box m={2}>
                                 <Button
                                     variant="contained"
