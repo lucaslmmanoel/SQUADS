8ae77b57caded7326b1c6825acc277ed1e9a369a
07087646141 <lucas.souza2@poupex.com.br>
2019-10-14 16:59:16 -0300

    :construction: iniciando a dinamização do form de hardskills

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactStars from "react-stars";
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import {Box, Card, Grid, InputLabel} from "@material-ui/core";
+import {
+    Box,
+    Card,
+    Grid,
+    InputLabel,
+    MenuItem,
+    makeStyles,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody, Table
+} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import SendIcon from "@material-ui/icons/Send";
 
+
 const HARDSKILLS = [
     {
-        'Desenvolvedor Javascript':
+        'id': 1,
+        'name': 'Desenvolvedor Python',
+        'abilities':
             [
                 {'a': 0},
                 {'b': 0},
@@ -19,7 +33,22 @@ const HARDSKILLS = [
             ]
     },
     {
-        'Desenvolvedor Python':
+        'id': 2,
+        'name': 'Desenvolvedor Javascript',
+        'abilities':
+            [
+                {'f': 0},
+                {'g': 0},
+                {'h': 0},
+                {'i': 0},
+                {'j': 0},
+                {'k': 0},
+            ]
+    },
+    {
+        'id': 3,
+        'name': 'Desenvolvedor PHP',
+        'abilities':
             [
                 {'a': 0},
                 {'b': 0},
@@ -30,7 +59,9 @@ const HARDSKILLS = [
             ]
     },
     {
-        'Desenvolvedor Ruby':
+        'id': 4,
+        'name': 'Desenvolvedor Ruby',
+        'abilities':
             [
                 {'a': 0},
                 {'b': 0},
@@ -41,29 +72,9 @@ const HARDSKILLS = [
             ]
     },
     {
-        'Desenvolvedor Ruby':
-            [
-                {'a': 0},
-                {'b': 0},
-                {'c': 0},
-                {'d': 0},
-                {'e': 0},
-                {'f': 0},
-            ]
-    },
-    {
-        'Desenvolvedor Python':
-            [
-                {'a': 0},
-                {'b': 0},
-                {'c': 0},
-                {'d': 0},
-                {'e': 0},
-                {'f': 0},
-            ]
-    },
-    {
-        'Desenvolvedor Java':
+        'id': 5,
+        'name': 'Desenvolvedor ADVPL',
+        'abilities':
             [
                 {'a': 0},
                 {'b': 0},
@@ -75,50 +86,83 @@ const HARDSKILLS = [
     },
 ]
 
-class HardSkills extends React.Component {
-    render() {
-        const ratingChanged = (newRating) => {
-            console.log(newRating)
-        }
-        return (
-            <>
-                <Grid container>
-                    <Grid item>
-                        <Card className='soft-skills'>
-                            <h1>Habilidades <br/> Profissionais</h1>
-                            <FormControl variant="outlined">
-                                <InputLabel ref={'inputLabel'} htmlFor="outlined-age-native-simple">
-                                    Age
-                                </InputLabel>
-                                <Select
-                                    native
-                                    value={0}
-                                    onChange={console.log('putz')}
-                                    inputProps={{
-                                        name: 'age',
-                                        id: 'outlined-age-native-simple',
-                                    }}
-                                >
-                                    <option value="" />
-                                    <option value={11}>TESTE B</option>
-                                </Select>
-                            </FormControl>
-                            <Box m={2}>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    endIcon={<SendIcon/>}
-                                >
-                                    Submeter para validação
-                                </Button>
-                            </Box>
 
-                        </Card>
-                    </Grid>
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+export default function HardSkills() {
+    const classes = useStyles();
+    const [state] = React.useState({
+        hardskill: '',
+        name: 'hai',
+        abilities: [] = [],
+    });
+    const [open, setOpen] = React.useState(false);
+
+    const handleChange = event => {
+        // Ao ser selecionada a profissão recuperar as habilidades da mesma
+        state.hardskill = event.target.value;
+        state.abilities = HARDSKILLS.filter(item => item.id === state.hardskill).map(skill => skill.abilities)
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const ratingChanged = newRating => {
+        console.log(newRating);
+    };
+    return (
+        <>
+            <Grid container>
+                <Grid item>
+                    <Card className='soft-skills'>
+                        <h1>Habilidades <br/> Profissionais</h1>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel htmlFor="demo-controlled-open-select">Profissão</InputLabel>
+                            <Select
+                                open={open}
+                                onClose={handleClose}
+                                onOpen={handleOpen}
+                                value={state.hardskill}
+                                onChange={handleChange}
+                                inputProps={{
+                                    name: 'hardskill',
+                                    id: 'demo-controlled-open-select',
+                                }}
+                            >
+                                {HARDSKILLS.map(skill => (
+                                    <MenuItem value={skill.id}>{skill.name}</MenuItem>
+                                ))}
+
+                            </Select>
+                        </FormControl>
+                        {state.abilities === [] ? console.log('cara não tem') : console.log('cara tem')}
+                        <Box m={2}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                endIcon={<SendIcon/>}
+                            >
+                                Submeter para validação
+                            </Button>
+                        </Box>
+                    </Card>
                 </Grid>
-            </>
-        );
-    }
+            </Grid>
+        </>
+    )
 }
-
-export default HardSkills;
